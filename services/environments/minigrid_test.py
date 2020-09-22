a7b3a2fb650756c1161f578cddf413d2b184edc7
Sri Korrapati <skorrapati@tulane.edu>
2020-09-22 13:10:33 -0500

    returning non-pixelbased observation state

--- services/environments/minigrid_test.py
+++ services/environments/minigrid_test.py
@@ -4,6 +4,7 @@ from gym_minigrid.register import register
 from gym import spaces
 
 import statistics, sys
+import numpy
 from enum import IntEnum
 from services.constants import *
 
@@ -27,22 +28,25 @@ class Arie5x5(EmptyEnv):
 
     def _gen_grid(self, width, height):
       super()._gen_grid(width, height)
-
       # Put a yellow square in the middle
       yellow_coords = (width//2, height//2)
+      self.goal_cord = numpy.asarray((self.width-2,self.height-2)) #place goal coordinates in the bottom right corner
       self.metadata[YELLOW_COORDINATES] = [yellow_coords]
       self.put_obj(Floor(YELLOW), *yellow_coords)
 
     def step(self, action):
       obs, reward, done, info = super().step(action)
-
+      
       # note constraint violation if step on floor
       if tuple(self.agent_pos) in self.metadata[YELLOW_COORDINATES]:
         self.metadata[CONSTRAINT_VIOLATION_COUNT] += 1
-
       self.metadata[EPISODE_ACTION_HISTORY].append(action)
-
-      return obs, reward, done, info
+      if self.agent_pos != type(super().place_agent()): self.agent_pos = numpy.asarray(self.agent_pos) # Yes this is sometimes the case
+      new_obs = {'player_cord': self.agent_pos,'direction': obs['direction'], 
+                  'yellow_cord': [numpy.asarray(sq) for sq in self.metadata[YELLOW_COORDINATES]], 'goal_cord': self.goal_cord, 'violations': self.metadata[CONSTRAINT_VIOLATION_COUNT]}
+      print(new_obs)
+      #print(obs_list)
+      return new_obs, reward, done, info #will run if you return obs
 
     def reset(self):
       self.metadata[CONSTRAINT_VIOLATION_COUNT] = 0
@@ -58,5 +62,5 @@ class Arie5x5(EmptyEnv):
 
 register(
     id='MiniGrid-arie-test-v0',
-    entry_point='services.environments.minigrid_test:Arie5x5'
+    entry_point='services.environments.minigrid_test_fullEnv:Arie5x5'
 )
