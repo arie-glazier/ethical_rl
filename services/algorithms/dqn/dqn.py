40f2b878af9c2e5f503165c8558a5490aece0ee2
arieglazier <arie.glazier@rasa.io>
2020-10-01 10:43:07 -0500

    refactor training step

--- services/algorithms/dqn/dqn.py
+++ services/algorithms/dqn/dqn.py
@@ -10,37 +10,13 @@ class  Algorithm(DQNBASE):
     def __init__(self, **kwargs):
       super().__init__(**kwargs)
 
-    def __training_step(self, episode_number):
-      states, actions, rewards, next_states, dones, weights, buffer_indexes = self.sample_experiences(episode_number)
-      next_Q_values = self.model.predict(next_states)
-
-      # Eli - this and the corresponding block in double_dqn
-      # needs to be the different (i.e at class level).  
-      # Each needs a method _get_target_q_values(self, next_q_values, rewards, dones)
+    def _get_target_q_values(self, next_Q_values, rewards, dones, *args):
       max_next_Q_values = np.max(next_Q_values, axis=1)
       target_Q_values = (rewards +
                           (1 - dones) * self.discount_factor * max_next_Q_values)
       target_Q_values = target_Q_values.reshape(-1, 1)
+      return target_Q_values
 
-      # TODO: Understand exactly what GradientTape does.
-      mask = tf.one_hot(actions, self.n_outputs)
-      with tf.GradientTape() as tape:
-        all_Q_values = self.model(states)
-        Q_values = tf.reduce_sum(all_Q_values * mask, axis=1, keepdims=True)
-        # TODO: check that multiplication of weights here is correct.
-        loss = tf.reduce_mean(weights * self.loss_function(target_Q_values, Q_values))
-      grads = tape.gradient(loss, self.model.trainable_variables)
-      self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
-
-      # this is TD error (i think)
-      # TODO: really think about this so we're sure it is the correct calculation
-      td_error = np.abs(np.subtract(Q_values.numpy().flatten(), target_Q_values))
-      # TODO: make this configurable
-      distribution_shape = 0.5
-      weighted_td_error = np.power(td_error, distribution_shape)
-
-      # update priority replay buffer
-      self.replay_buffer.update_priorities(buffer_indexes, weighted_td_error)
 
     def train(self):
         rewards = []
@@ -55,7 +31,7 @@ class  Algorithm(DQNBASE):
                     break
 
             if episode >= self.buffer_wait_steps:  # no need to train until the buffer has data
-                self.__training_step(episode)
+              self._training_step(episode)
 
             # print(f"episode: {episode} / total_rewards: {total_episode_rewards} / total_steps: {step} / metadata: {self.env.metadata}")
             rewards.append(total_episode_rewards)
